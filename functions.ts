fe3e75e9b08cac0b5b562a0fcb71e9cb8d9a3d0b
Jovakinn <max05012004@gmail.com>
2021-09-26 02:52:32 +0300

    add functions overloading realization

--- functions.ts
+++ functions.ts
@@ -5,3 +5,35 @@ function add(a: number, b: number): number {
 function toUpperCase(str: string): string {
     return str.trim().toUpperCase()
 }
+
+interface MyPosition {
+    x: number | undefined
+    y: number | undefined
+}
+
+interface MyPositionWithDefault extends MyPosition {
+    default: string
+}
+
+function position(): MyPosition
+function position(a: number): MyPositionWithDefault
+function position(a: number, b: number): MyPosition
+
+function position(a?: number, b?: number) {
+    if (!a && !b) {
+        return {x: undefined, y: undefined}
+    }
+
+    if (a && !b) {
+        return {x: a, y: undefined}
+    }
+
+    if (a && b) {
+        return {x: a, y: b}
+    }
+
+    return {x: a, y: b}
+}
+console.log('Empty: ', position())
+console.log('1 param: ', position(121))
+console.log('2 param: ', position(122, 123))
