70cf08ef46674cd2a1bc449f85f04ad709c22afa
Jovakinn <max05012004@gmail.com>
2021-09-26 03:07:00 +0300

    add classes realizations

---
+++ classes.ts
@@ -0,0 +1,60 @@
+class Typescript {
+    version: string
+
+    constructor(version: string) {
+        this.version = version;
+    }
+    info(name: string) {
+        return '[${name}]: Typescript version is ${this.version}'
+    }
+}
+
+class Car {
+    readonly model: string
+    readonly numberOfWheels: number = 4
+
+    constructor(model: string, numberOfWheels: number) {
+        this.model = model;
+        this.numberOfWheels = numberOfWheels;
+    }
+}
+
+//====================
+class Animal {
+    protected voice: string = ''
+    public color: string = 'black'
+
+    constructor(voice: string, color: string) {
+        this.voice = voice;
+        this.color = color;
+    }
+
+    private static go() {
+        console.log('Go')
+    }
+}
+
+class Cat extends Animal {
+    public setVoice(voice: string): void {
+        this.voice = voice
+    }
+}
+
+const cat = new Cat("meaww", "White")
+console.log(cat.color.valueOf().toUpperCase())
+// ============================
+
+abstract class Component {
+    abstract render(): void
+    abstract info(): string
+}
+
+class AppComponent extends Component {
+    info(): string {
+        return "Info.";
+    }
+
+    render(): void {
+        console.log("Component of render")
+    }
+}
