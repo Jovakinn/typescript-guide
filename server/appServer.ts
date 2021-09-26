085287169d4f2cbf050df7f443cc31403d0e21f6
Jovakinn <max05012004@gmail.com>
2021-09-26 23:09:38 +0300

    implemented request checking

--- server/appServer.ts
+++ server/appServer.ts
@@ -1,10 +1,18 @@
 import * as http from "http";
 import * as url from "url";
 
-http.createServer((req, res) => {
-    res.end("Hello NodeJS + TS!")
-    console.log(req.method)
-    let urlRequest = url.parse(<string>req.url?.trim().toString(), true)
-    console.log(urlRequest)
+http.createServer((request, response) => {
     console.log('Server has started...')
+
+    if (request.method == 'GET') {
+        response.end("Hello NodeJS + TS!")
+        console.log(request.method)
+        let urlRequest = url.parse(<string>request.url?.trim().toString(), true)
+        //console.log(urlRequest)
+        console.log(urlRequest.query.test)
+    }
+
+    if (request.method == 'POST') {
+
+    }
 }).listen(3000);
