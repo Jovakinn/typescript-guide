b395f88712f18d85618f90cff365abcc3879f871
Jovakinn <max05012004@gmail.com>
2021-09-26 18:53:51 +0300

    add server implementation

--- server/appServer.ts
+++ server/appServer.ts
@@ -1,6 +1,10 @@
 import * as http from "http";
+import * as url from "url";
 
 http.createServer((req, res) => {
     res.end("Hello NodeJS + TS!")
+    console.log(req.method)
+    let urlRequest = url.parse(<string>req.url?.trim().toString(), true)
+    console.log(urlRequest)
     console.log('Server has started...')
 }).listen(3000);
