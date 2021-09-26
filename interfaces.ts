5a7eb1770c052e2ae9f7b2c53f74f44adf36b514
Jovakinn <max05012004@gmail.com>
2021-09-26 14:59:17 +0300

    add OOP principles realization

--- interfaces.ts
+++ interfaces.ts
@@ -7,6 +7,67 @@ interface Rect {
     }
 }
 
+interface Shape {
+    readonly id: ID
+    side1?: number
+    side2?: number
+    side3?: number
+    areaCalculate(): void
+}
+
+class Triangle implements Shape {
+    readonly id: ID = getRandomInt(50, 500);
+
+    areaCalculate(): void {
+        console.log("area")
+    }
+
+    get side1(): number {
+        return this.side1;
+    }
+
+    set side1(value: number) {
+        this.side1 = value;
+    }
+
+    get side2(): number {
+        return this.side2;
+    }
+
+    set side2(value: number) {
+        this.side2 = value;
+    }
+
+    get side3(): number {
+        return this.side3;
+    }
+
+    set side3(value: number) {
+        this.side3 = value;
+    }
+}
+const myFavoriteTriangle: Triangle = {
+    id: getRandomInt(1, 2),
+    get side1(): number {
+        return 0;
+    }, get side2(): number {
+        return 0;
+    }, get side3(): number {
+        return 0;
+    },
+    areaCalculate(): void {
+        console.log("area of my triangle")
+    }
+}
+
+function getRandomInt(min: number, max: number) : number{
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+
+
 const rect1: Rect = {
     color: "#ccc",
     id: "1",
