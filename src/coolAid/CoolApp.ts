9e8e2e04b2eee989046c0a5b4efaa846b2543d6d
Jovakinn <max05012004@gmail.com>
2021-09-27 21:26:08 +0300

    add logInfo functional

--- src/coolAid/CoolApp.ts
+++ src/coolAid/CoolApp.ts
@@ -4,6 +4,13 @@ class Person {
 const max = new Person("Max")
 const btn: Element | null = document.querySelector('#btn')
 
+let anyFlag
+
+function logINfo(data: string) {
+    console.log(data);
+    anyFlag = true
+    console.log(anyFlag)
+}
 
 if (btn) {
     btn.addEventListener('click', () => {
