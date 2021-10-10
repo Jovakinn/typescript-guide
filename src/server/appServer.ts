7a1b650e04517835c90b0b212fc45078335b8758
Jovakinn <max05012004@gmail.com>
2021-10-10 22:58:00 +0300

    add server implementation

--- src/server/appServer.ts
+++ src/server/appServer.ts
@@ -1,18 +1,12 @@
-import * as http from "http";
-import * as url from "url";
+import express from 'express';
 
-http.createServer((request, response) => {
-    console.log('Server has started...')
+const app = express();
+const PORT = 3000;
 
-    if (request.method == 'GET') {
-        response.end("Hello NodeJS + TS!")
-        console.log(request.method)
-        let urlRequest = url.parse(<string>request.url?.trim().toString(), true)
-        //console.log(urlRequest)
-        console.log(urlRequest.query.test)
-    }
+app.get('/', ((req, res) => {
+    res.send('Hello Node.js!')
+}));
 
-    if (request.method == 'POST') {
-
-    }
-}).listen(3000);
+app.listen(PORT,() => {
+    return console.log(`server is running on ${PORT}`);
+})
