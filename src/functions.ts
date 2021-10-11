ee65bab93a41193c759fcecf8431e63ad41c4906
Jovakinn <max05012004@gmail.com>
2021-10-11 23:03:56 +0300

    add classes and interface Schooler implementation

--- src/functions.ts
+++ src/functions.ts
@@ -11,10 +11,75 @@ interface MyPosition {
     y: number | undefined
 }
 
+type typeToShow = Schooler
+
+interface Showable {
+    valueToShow(value: Schooler): typeToShow
+}
+
 interface MyPositionWithDefault extends MyPosition {
     default: string
 }
+enum Sex {
+    MALE,
+    FEMALE,
+    QUIR
+}
 
+class Schooler implements Showable {
+    private _firstName: string
+    private _secondName: string
+    private _age: number
+    private _sex: Sex
+
+    constructor(firstName: string, secondName: string, age: number, sex: Sex) {
+        this._firstName = firstName;
+        this._secondName = secondName;
+        this._age = age;
+        this._sex = sex;
+    }
+
+    get firstName(): string {
+        return this._firstName;
+    }
+
+    set firstName(value: string) {
+        this._firstName = value;
+    }
+
+    get secondName(): string {
+        return this._secondName;
+    }
+
+    set secondName(value: string) {
+        this._secondName = value;
+    }
+
+    get age(): number {
+        return this._age;
+    }
+
+    set age(value: number) {
+        this._age = value;
+    }
+
+    get sex(): Sex {
+        return this._sex;
+    }
+
+    set sex(value: Sex) {
+        this._sex = value;
+    }
+
+    valueToShow(value: Schooler): typeToShow {
+        return value;
+    }
+}
+
+function SchoolarGreate() {
+    const schooler = new Schooler("Max", "Khodakov", 17, Sex.MALE);
+    console.log(schooler.valueToShow(schooler));
+}
 function position(): MyPosition
 function position(a: number): MyPositionWithDefault
 function position(a: number, b: number): MyPosition
@@ -37,3 +102,4 @@ function position(a?: number, b?: number) {
 console.log('Empty: ', position())
 console.log('1 param: ', position(121))
 console.log('2 param: ', position(122, 123))
+SchoolarGreate();
