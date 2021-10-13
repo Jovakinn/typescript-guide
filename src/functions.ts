25083998b0361f9fdbe3702bb68d78223c02a816
Jovakinn <max05012004@gmail.com>
2021-10-14 01:40:14 +0300

    add move function

--- src/functions.ts
+++ src/functions.ts
@@ -172,6 +172,17 @@ function multiplyValues(container: Container, factor: number) {
     }
 }
 
+type Fish = { swim: () => void };
+type Bird = { fly : () => void };
+type Human = { swim?: () => void; fly?: () => void };
+
+function move(animal: Fish | Bird | Human) {
+    if ("swim" in animal) {
+        animal;
+    } else {
+        animal;
+    }
+}
 
 
 console.log('Empty: ', position())
