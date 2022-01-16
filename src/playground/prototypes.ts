45398b2c839ca1dbfb1dbca4486d3df1cfe7b5d7
Jovakinn <max05012004@gmail.com>
2022-01-16 23:41:44 +0200

    add currying

--- src/playground/prototypes.ts
+++ src/playground/prototypes.ts
@@ -1,3 +1,5 @@
+import { create } from "domain";
+
 function Point(this: any, x: number, y: number) {
     this.x = x;
     this.y = y;
@@ -31,3 +33,39 @@ p.move(-5, 10);
 console.log(p);
 console.log(p.toString());
 console.log(p + '');
+
+const log = (base: number, n: number) => Math.log(n) / Math.log(base);
+const createLog = (base: number) => (n: number) => log(base, n);
+
+const lg = createLog(10);
+const ln = createLog(Math.E);
+
+console.log('lg(5) = ' + lg(5));
+console.log('ln(5) = ' + ln(5));
+
+
+const partial = (fn: any, x: any) => (...args: any[]) => fn(x, ...args);
+
+const sum4 = (a: number, b: number, c: number, d: number) => (a + b + c + d);
+
+const f11 = partial(sum4, 1);
+const f12 = partial(f11, 2);
+const f13 = partial(f12, 3);
+
+const y1 = f13(4);
+console.log(y1);
+
+// currying
+
+const curry = (fn: any) => (...args: any) => {
+    if (fn.length > args.length) {
+        const f = fn.bind(null, ...args);
+        return curry(f);
+    } else {
+        return fn(...args);
+    }
+}
+
+const f = curry(sum4);
+const test = f(1)(2)(3)(4);
+console.log(test);
